7489a7c5b4a53dda432791209c5ee23ba66894b1
Robin Rohwer <robin.rohwer@gmail.com>
2022-12-21 19:06:01 -0800

    updated the plot but still don't know how to adjust white space in margins or stupid double legend that's showing up.

--- src/5b_anoxia_and_biomass.R
+++ src/5b_anoxia_and_biomass.R
@@ -8,19 +8,20 @@ library(tidyverse)
 library(patchwork)
 
 phyto <- readRDS("data_processed/3a_phyto_list.rds")
-anox <- readRDS("data_input/14-DO_profiles_with_more_oxycline_stats-colnames_updated.rds") # pull from data I curated elsewhere- just quick for now
+anox <- readRDS("data_processed/0n_combined_DO-full_profiles.rds") # pull from data I curated elsewhere- just quick for now
 strat <- readRDS("data_processed/3g_stratification_dates.rds")
+strat <- strat$mean
 
 plot.file <- "figs_publication/Fig5-robin.pdf"
 
-# ---- format data ----
+col.pre <- adjustcolor("steelblue",.9)
+col.post <- "orange3"
 
-strat <- strat$mean
+# ---- format phyto ----
 
 phyto <- phyto$tot
 phyto$yday <- yday(phyto$date)
 phyto$year <- year(phyto$date)
-phyto <- phyto[phyto$year %in% strat$year, ]
 phyto$days.since.strat <- NA
 for (yr in strat$year){
   phyto$days.since.strat[phyto$year == yr] <- phyto$yday[phyto$year == yr] - strat$start[strat$year == yr]
@@ -29,32 +30,73 @@ phyto$invasion <- "pre"
 phyto$invasion[phyto$year >= 2010] <- "post"
 head(phyto)
 
-anox <- anox[ ,c("Sample.DateTime","Top.Anoxic")]
-head(anox)
-anox <- aggregate(x = anox$Top.Anoxic, by = list(anox$Sample.DateTime), FUN = mean, na.rm = T)
-head(anox)
-anox <- anox[!is.na(anox$x), ]
-anox$x[abs(anox$x) > 20] <- 20 #  make max depth 20 to match MO profiles
-colnames(anox) <- c("date","top.anoxic")
-anox$yday <- yday(anox$date)
-anox$year <- year(anox$date)
-anox <- anox[anox$year %in% strat$year, ]
+# ---- remove spring anoxia sketchy ones ----
+
+# 2007-5-14
+# 2015-5-24
+# 2015-5-28
+
+index <- anox$Year == 2007 & anox$Month == 5 & anox$Day == 14
+plot(x = anox$DO.mg.L[index], y = -anox$Depth.m[index])
+# profile looks fine, but clearly stratified to ~ 10 m when the lake supposedly just stratified 8.75 days earlier
+# so removing because I think there is no explanation for how this cannot be wrong?
+anox <- anox[!index, ]
+
+index <- anox$Year == 2015 & anox$Month == 5 & anox$Day == 24
+plot(x = anox$DO.mg.L[index], y = -anox$Depth.m[index])
+# looks like shit, how did that make it past my screening
+anox <- anox[!index, ]
+
+index <- anox$Year == 2015 & anox$Month == 5 & anox$Day == 28
+plot(x = anox$DO.mg.L[index], y = -anox$Depth.m[index])
+# obviously something wrong with the probe
+anox <- anox[!index, ]
+
+# ---- remove summer sketchy depth = 0 ones ----
+
+# 2013-07-29
+# 2016-07-06
+# 2016-07-08
+
+index <- anox$Year == 2013 & anox$Month == 7 & anox$Day == 29
+plot(x = anox$DO.mg.L[index], y = -anox$Depth.m[index])
+# super wack, how did this make it past screening I freaking already checked them all
+anox <- anox[!index, ]
+
+index <- anox$Year == 2016 & anox$Month == 7 & anox$Day == 06
+plot(x = anox$DO.mg.L[index], y = -anox$Depth.m[index])
+# super wack, how did this make it past screening I freaking already checked them all
+anox <- anox[!index, ]
+
+index <- anox$Year == 2016 & anox$Month == 7 & anox$Day == 08
+plot(x = anox$DO.mg.L[index], y = -anox$Depth.m[index])
+# super wack, how did this make it past screening I freaking already checked them all
+anox <- anox[!index, ]
+
+# ---- format anox ----
+
+anox$yday <- yday(anox$Date)
+anox <- anox[anox$Depth.m <= 20, ] # trim profiles to extend to 20 m only
 anox$days.since.strat <- NA
 for (yr in strat$year){
-  anox$days.since.strat[anox$year == yr] <- anox$yday[anox$year == yr] - strat$start[strat$year == yr]
+  anox$days.since.strat[anox$Year == yr] <- anox$yday[anox$Year == yr] - strat$start[strat$year == yr]
 }
-anox$invasion <- "pre"
-anox$invasion[anox$year >= 2010] <- "post"
+anox <- anox[order(anox$Date), ]
+
+anox <- anox |> 
+  group_by(Date) |> 
+  filter(DO.mg.L < 1.5) |> 
+  filter(Depth.m == min(Depth.m))  |> 
+  mutate(invasion = if_else(Year < 2010, 'Pre','Post'))
 head(anox)
-summary(anox$top.anoxic)
 
 # ---- plot, attempt ggplot - align by strat ----
 
-p.anox <- ggplot(data = anox, aes(x = days.since.strat, y = top.anoxic, color = invasion))+
-  scale_fill_manual(values = c("orange3", "steelblue"))+
-  scale_color_manual(values = c("orange3", "steelblue"))+
+p.anox <- ggplot(data = anox[!is.na(anox$days.since.strat), ], aes(x = days.since.strat, y = Depth.m, color = invasion))+
+  scale_fill_manual(values = c(col.post, col.pre))+
+  scale_color_manual(values = c(col.post, col.pre))+
   geom_point(aes(fill = invasion), shape = 21, color = "black", size = 1, alpha = .5)+
-  geom_path(aes(group = year), alpha = .5)+
+  geom_path(aes(group = Year), alpha = .5)+
   geom_smooth(aes(group = invasion, fill = invasion), method = "loess", alpha = .6)+
   theme_bw()+
   theme(panel.grid = element_blank())+
@@ -62,42 +104,52 @@ p.anox <- ggplot(data = anox, aes(x = days.since.strat, y = top.anoxic, color = 
   scale_y_reverse()
 
 p.phyto <- ggplot(data = phyto, aes(x = days.since.strat, y = biomass, color = invasion))+
-  scale_fill_manual(values = c("orange3", "steelblue"))+
-  scale_color_manual(values = c("orange3", "steelblue"))+
+  scale_fill_manual(values = c(col.post, col.pre))+
+  scale_color_manual(values = c(col.post, col.pre))+
   geom_point(aes(fill = invasion), shape = 21, color = "black", size = 1, alpha = .5)+
   geom_path(aes(group = year),alpha = .5)+
   geom_smooth(aes(group = invasion, fill = invasion), method = "loess", alpha = .6)+
   theme_bw()+
   theme(panel.grid = element_blank())+
-  coord_cartesian(xlim = c(-100,200), ylim = c(0,25))
+  coord_cartesian(xlim = c(-100,200), ylim = c(.001,25))+
+  scale_y_continuous(trans = "log10", name = "log biomass (mg/L)")
+
+p.anox / p.phyto
 
 # ---- plot, attempt ggplot - align by date ----
 
-p.anox <- ggplot(data = anox, aes(x = yday, y = top.anoxic, color = invasion))+
-  scale_fill_manual(values = c("orange3", "steelblue"))+
-  scale_color_manual(values = c("orange3", "steelblue"))+
+x.lab.yday <- yday(parse_date_time(x = paste(2:11,1), orders = "md"))
+x.lab <- month(parse_date_time(x = paste(2:11,1), orders = "md"), label = T)
+
+p.anox <- ggplot(data = anox, aes(x = yday, y = Depth.m, color = invasion))+
+  scale_fill_manual(values = c(col.post, col.pre))+
+  scale_color_manual(values = c(col.post, col.pre))+
   geom_point(aes(fill = invasion), shape = 21, color = "black", size = 1, alpha = .5)+
-  geom_path(aes(group = year), alpha = .5)+
-  geom_smooth(aes(group = invasion, fill = invasion), method = "loess", alpha = .6)+
+  geom_path(aes(group = Year), alpha = .5)+
+  geom_smooth(aes(group = invasion, fill = invasion), method = "loess", alpha = .7)+
   theme_bw()+
   theme(panel.grid = element_blank())+
-  coord_cartesian(xlim = c(150,350), ylim = c(20,6), expand = F)+
+  coord_cartesian(xlim = c(150,300), ylim = c(20,6), expand = F)+
+  scale_x_continuous(name = element_blank(), breaks = x.lab.yday, labels = x.lab)+
   scale_y_reverse()+
-  scale_y_continuous(name = "Depth anoxic transition (m)")
+  scale_y_continuous(name = "Depth Anoxic Layer (m)")
 
 p.phyto <- ggplot(data = phyto, aes(x = yday, y = biomass, color = invasion))+
-  scale_fill_manual(values = c("orange3", "steelblue"))+
-  scale_color_manual(values = c("orange3", "steelblue"))+
+  scale_fill_manual(values = c(col.post, col.pre))+
+  scale_color_manual(values = c(col.post, col.pre))+
   geom_point(aes(fill = invasion), shape = 21, color = "black", size = 1, alpha = .5)+
   geom_path(aes(group = year),alpha = .5)+
-  geom_smooth(aes(group = invasion, fill = invasion), method = "loess", alpha = .6)+
+  geom_smooth(aes(group = invasion, fill = invasion), method = "loess", alpha = .7)+
   theme_bw()+
   theme(panel.grid = element_blank())+
-  coord_cartesian(xlim = c(100,350), ylim = c(.01,25))+
-  scale_y_continuous(trans = "log10", name = "log biomass (mg/L)")
+  coord_cartesian(xlim = c(50,300), ylim = c(.01,25))+
+  scale_x_continuous(name = element_blank(), breaks = x.lab.yday, labels = x.lab)+
+  scale_y_continuous(trans = "log10", name = "log Phytoplankton Biomass (log(mg/L))")
 
 # save plot ----
 
 pdf(file = plot.file, width = 6.5, height = 3)
-p.anox + p.phyto + plot_layout(ncol = 2, guides = "collect")
+
+p.phyto + p.anox + plot_layout(ncol = 2, widths = c(2.1,1), guides = "collect")
+
 dev.off()
